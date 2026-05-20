03b31ffd432e824019d924a898c3b3dc9587ad99
jharchana123-web <jharchana123@gmail.com>
2026-05-20 15:28:39 +0530

    Day 1 Project

---
+++ crazy.py
@@ -0,0 +1,11 @@
+Name = input("Enter Your Name: ")
+print(f"Hello Sir,{Name}") 
+print("=========Let us calculate!==========")
+
+num1 = int(input("Enter Number 1: "))
+num2 = int(input("Enter Number 2: "))
+num3 = int(input("Enter Number 3: "))
+num4 = (input("Enter Number 4: "))
+
+Sum = num1 + num2 + num3 + num4
+print(f"The sum is: {Sum}")
