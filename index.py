fcecbd2bb43426971559685e9c5593dd1b798091
jharchana123-web <jharchana123@gmail.com>
2026-05-20 20:37:58 +0530

    My third task of the day.

---
+++ index.py
@@ -0,0 +1,17 @@
+import time
+
+name = input("What is your name: ")
+
+Name = (f"Hi! {name}, let's do some crazy!")
+print(f"Hello! miracle is going on my friend {name}!")
+time.sleep(5)
+
+print (f"Acess Granted {name}")
+
+print(f"Welcome back my agent {name}!")
+
+
+
+
+
+
